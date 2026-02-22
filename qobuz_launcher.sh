b7644033f3599dbc202afda90c75350f0fa57d84
logon84 <logon84@users.noreply.github.com>
2026-02-22 23:06:03 +0100

    Bump Qobuz launcher script version to 1.8
    Updated version number and year in the Qobuz launcher script.

--- qobuz_launcher.sh
+++ qobuz_launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Qobuz launcher script v 1.7 @Logon84 2025
+# Qobuz launcher script v 1.8 @Logon84 2026
 
 #To use Qobuz with wine and ALSA sound, disable "Exclusive mode" inside qobuz app.
 #If you want to run Qobuz with exclusive mode under linux, either run this script with "-exclusive" flag or run the script while holding shift key.
@@ -33,55 +33,28 @@ if [ "$1" = "-exclusive" ] || [ ! -z "$shift_pressed" ]; then
              "Qobuz-wine" "Launching Qobuz in HW Exclusive Mode" [] \
             "{'transient': <true>}" 5000 > /dev/null 2>&1
 
+    #get device to play to and make it exclusive
+    speakers_sink_name=$(wpctl status -n | grep -zoP '(?<=Sinks:)(?s).*?(?=├─)' | grep -a '*' | awk '{print ($4)}')
+    speakers_alsa_id=$(pw-dump | jq '.[] | select(.info.props."node.name"=="'"${speakers_sink_name}"'") | .info.props."device.id"')
+    speakers_audio_n=$(pw-dump | jq '.[] | select(.info.props."object.id"=='"${speakers_alsa_id}"') | .info.props."api.dbus.ReserveDevice1"' | tr -d "\"")
+    pw-reserve -r -n $speakers_audio_n &
+    pw_res_pid=$!
+
     #launch Qobuz
     env WINEPREFIX=$wine_prefix WINEDEBUG=fixme-winediag $(dirname $wine_binary)/winecfg -v win7
     env WINEPREFIX=$wine_prefix WINEDEBUG=fixme-winediag $wine_binary C:\\users\\$wine_username\\AppData\\Local\\Qobuz\\Qobuz.exe > $wine_prefix/drive_c/users/$wine_username/AppData/Roaming/Qobuz/wine.log 2>&1
 
-    #get pipewire object id associated with Qobuz wine node
-    qobuz_pid=""
-    while [ -z "$qobuz_pid" ]; do
-        sleep 0.1
-        qobuz_pid=$(ps -e -o pid,comm | grep CrBrowserMain | awk {'print$1'})
-    done
-    qobuz_pipewire_interface_id=$(pw-dump | jq '.[] | select(.info.props."pipewire.sec.pid"=='"${qobuz_pid}"') | .info.props."object.id"')
-    qobuz_wine_stream_id=$(pw-dump | jq '.[] | select(.info.props."client.id"=='"${qobuz_pipewire_interface_id}"') | .info.props."object.id"')
-
-    #get current default sink id
-    speakers_sink_id=$(wpctl status -n | grep -zoP '(?<=Sinks:)(?s).*?(?=├─)' | grep -a '*' | awk '{print ($3+0)}')
-
-    # Create a new null output sink
-    pw-cli create-node adapter '{ factory.name=support.null-audio-sink node.nick='"${qobuz_wine_stream_id}@${speakers_sink_id}"' node.name="Null Output" node.description="Null Output" media.class=Audio/Sink object.linger=true audio.position=[FL FR] }'
-    null_sink_id=$(wpctl status | grep "Null Output" | tr -d "*" | awk '{print ($2+0)}')
-
-    #set null output as default
-    wpctl set-default $null_sink_id
-
-    #run autolinker script
-    wpexec "$HOME/.config/wireplumber/scripts/steam-wire.lua" &
-    lua_script_pid=$!
-
-    #link Qobuz wine node to output sink only first time (next time is automatic by lua script)
-    in_port1=$(pw-dump | jq '.[] | select(.info.props."node.id"=='"${speakers_sink_id}"' and .info.props."port.direction"=="in") | .info.props."port.id"' | head -n 1 | tr -d "\"")
-    in_port2=$(pw-dump | jq '.[] | select(.info.props."node.id"=='"${speakers_sink_id}"' and .info.props."port.direction"=="in") | .info.props."port.id"' | tail -n 1 | tr -d "\"")
-    
-    out_port1=$(pw-dump | jq '.[] | select(.info.props."node.id"=='"${qobuz_wine_stream_id}"' and .info.props."port.direction"=="out") | .info.props."port.id"' | head -n 1 | tr -d "\"")
-    out_port2=$(pw-dump | jq '.[] | select(.info.props."node.id"=='"${qobuz_wine_stream_id}"' and .info.props."port.direction"=="out") | .info.props."port.id"' | tail -n 1 | tr -d "\"")
-
-    pw-cli create-link $qobuz_wine_stream_id $out_port1 $speakers_sink_id $in_port1 object.linger=1
-    pw-cli create-link $qobuz_wine_stream_id $out_port2 $speakers_sink_id $in_port2 object.linger=1
-
     #Wait for closing qobuz
     ( tail -f -n0 $wine_prefix/drive_c/users/$wine_username/AppData/Roaming/Qobuz/wine.log & ) | grep -Eq "$exit_text"
     echo -e "\n\n\n\nClosing....\n\n\n\n"
-
-    #destroy null output virtual device
-    pw-cli destroy $null_sink_id
-
-    #set default hw as it was before opening Qobuz
-    wpctl set-default $speakers_sink_id
-
-    #kill lua script
-    kill -9 $lua_script_pid
+    
+    #destroy device exclusive mode & restore default pipewire device
+    kill -15 $pw_res_pid 2> /dev/null
+    wait $pw_res_pid 2> /dev/null
+    systemctl --user restart pipewire pipewire-pulse wireplumber
+    sleep 2
+    past_default_new_id=$(wpctl status -n | grep -a $speakers_sink_name | grep vol | awk '{print ($3+0)}')
+    wpctl set-default $past_default_new_id
 
 else
     #Notify
